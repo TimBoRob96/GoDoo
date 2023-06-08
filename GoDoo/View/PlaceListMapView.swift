3bf822905915f895bbe955e73a2cad0cfa11e827
Timothy Roberts <timbooyah@gmail.com>
2023-06-08 18:15:15 +0100

    Added navigation links and place marker titles to place map view

--- GoDoo/View/PlaceListMapView.swift
+++ GoDoo/View/PlaceListMapView.swift
@@ -25,7 +25,16 @@ struct PlaceListMapView: View {
         
         if placesManager.region != nil {
             Map(coordinateRegion: $region, annotationItems: placesManager.placesList) { placeMark in
-                MapMarker(coordinate: placeMark.coordinate)
+                
+                MapAnnotation(coordinate: placeMark.coordinate) {
+                  NavigationLink {
+                    PlaceView(place: placeMark)
+                  } label: {
+                    PlaceAnnotationView(title: placeMark.placeName)
+                  }
+                }
+                //MapMarker(coordinate: placeMark.coordinate)
+                //Text(placeMark.placeName)
                 
             }            .onAppear {
                 //region = placesManager.region!
@@ -42,3 +51,38 @@ struct PlaceListMapView: View {
 //        PlaceListMapView()
 //    }
 //}
+
+// code sourced from https://kristaps.me/blog/swiftui-map-annotations/
+// To customize later on but this was exactly what I was looking for!
+
+
+struct PlaceAnnotationView: View {
+  @State private var showTitle = true
+  
+  let title: String
+  
+  var body: some View {
+    VStack(spacing: 0) {
+      Text(title)
+        .font(.callout)
+        .padding(5)
+        .background(Color(.white))
+        .cornerRadius(10)
+        //.opacity(showTitle ? 0 : 1)
+      
+      Image(systemName: "mappin.circle.fill")
+        .font(.title)
+        .foregroundColor(.red)
+      
+      Image(systemName: "arrowtriangle.down.fill")
+        .font(.caption)
+        .foregroundColor(.red)
+        .offset(x: 0, y: -5)
+    }
+//    .onTapGesture {
+//      withAnimation(.easeInOut) {
+//        showTitle.toggle()
+//      }
+//    }
+  }
+}
